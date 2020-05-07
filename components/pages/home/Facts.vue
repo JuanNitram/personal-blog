91368a11402ee1deac1a93e7bf18f2309d48e173
Juan <juan@lightit.io>
2020-05-07 19:46:31 -0300

    Update version

--- components/pages/home/Facts.vue
+++ components/pages/home/Facts.vue
@@ -1,12 +1,10 @@
 <template>
     <div class="mx-auto px-2 lg:px-32 pb-12 text-gray-400">
         <div class="w-full px-5 py-4">
-            <h1 class="text-2xl font-bold text-center lg:text-left">
-                <span class="border-b-2 border-blue-800 text-gray-100">Fun Facts</span>
-            </h1>
+            <Title text="Fun facts"/>
         </div>
         <div class="flex flex-col lg:flex-row">
-            <div class="w-full lg:w-1/4 p-5">
+            <div class="w-full lg:w-1/4 px-10 py-5 lg:px-5 lg:py-5">
                 <div class="bg-gray-800 border-2 border-gray-800 rounded-md p-4 flex flex-col justify-between items-center">
                     <i class="far fa-lightbulb fa-5x text-blue-800"></i>
                     <p class="text-center pt-8 pb-4">
@@ -14,7 +12,7 @@
                     </p>
                 </div>
             </div>
-            <div class="w-full lg:w-1/4 p-5">
+            <div class="w-full lg:w-1/4 px-10 py-5 lg:px-5 lg:py-5">
                 <div class="bg-gray-800 border-2 border-gray-800 rounded-md p-4 flex flex-col justify-between items-center">
                     <i class="far fa-lightbulb fa-5x text-blue-800"></i>
                     <p class="text-center pt-8 pb-4">
@@ -22,7 +20,7 @@
                     </p>
                 </div>
             </div>
-            <div class="w-full lg:w-1/4 p-5">
+            <div class="w-full lg:w-1/4 px-10 py-5 lg:px-5 lg:py-5">
                 <div class="bg-gray-800 border-2 border-gray-800 rounded-md p-4 flex flex-col justify-between items-center">
                     <i class="far fa-lightbulb fa-5x text-blue-800"></i>
                     <p class="text-center pt-8 pb-4">
@@ -30,7 +28,7 @@
                     </p>
                 </div>
             </div>
-            <div class="w-full lg:w-1/4 p-5">
+            <div class="w-full lg:w-1/4 px-10 py-5 lg:px-5 lg:py-5">
                 <div class="bg-gray-800 border-2 border-gray-800 rounded-md p-4 flex flex-col justify-between items-center">
                     <i class="far fa-lightbulb fa-5x text-blue-800"></i>
                     <p class="text-center pt-8 pb-4">
@@ -41,3 +39,13 @@
         </div>
     </div>
 </template>
+
+<script>
+    import Title from '../../shared/Title';
+
+    export default {
+        components: {
+            Title
+        }    
+    }
+</script>
--- components/pages/home/Services.vue
+++ components/pages/home/Services.vue
@@ -1,34 +1,32 @@
 <template>
     <div id="services" class="mx-auto px-2 lg:px-32 text-gray-400">
         <div class="w-full px-5 pb-4">
-            <h1 class="text-2xl font-bold text-center lg:text-left">
-                <span class="border-b-2 border-blue-800 text-gray-100">What I Do</span>
-            </h1>
+            <Title text="What I do"/>
         </div>
-        <div class="flex flex-col lg:flex-row">
-            <div class="w-full lg:w-1/2 p-5 flex flex-col lg:flex-row justify-between items-center">
-                <i class="far fa-file-code fa-3x pb-5 lg:pb-0 text-blue-800 text-blue-800"></i>
-                <p class="px-0 lg:px-8 text-center lg:text-left">
+        <div class="block lg:flex">
+            <div class="w-full lg:w-1/2 p-5 flex justify-between">
+                <i class="far fa-file-code fa-3x px-5 lg:px-0 lg:pb-0 text-blue-800 text-blue-800"></i>
+                <p class="px-0 lg:px-8 text-left">
                     Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.
                 </p>
             </div>
-            <div class="w-full lg:w-1/2 p-5 flex flex-col lg:flex-row justify-between items-center">
-                <i class="far fa-file-code fa-3x pb-5 lg:pb-0 text-blue-800"></i>
-                <p class="px-0 lg:px-8 text-center lg:text-left">
+            <div class="w-full lg:w-1/2 p-5 flex justify-between">
+                <i class="far fa-file-code fa-3x px-5 lg:px-0 lg:pb-0 text-blue-800 text-blue-800"></i>
+                <p class="px-0 lg:px-8 text-left">
                     Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.
                 </p>
             </div>
         </div>
-        <div class="flex flex-col lg:flex-row">
-            <div class="w-full lg:w-1/2 p-5 flex flex-col lg:flex-row justify-between items-center">
-                <i class="far fa-file-code fa-3x pb-5 lg:pb-0 text-blue-800"></i>
-                <p class="px-0 lg:px-8 text-center lg:text-left">
+        <div class="block lg:flex">
+            <div class="w-full lg:w-1/2 p-5 flex justify-between">
+                <i class="far fa-file-code fa-3x px-5 lg:px-0 lg:pb-0 text-blue-800 text-blue-800"></i>
+                <p class="px-0 lg:px-8 text-left">
                     Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.
                 </p>
             </div>
-            <div class="w-full lg:w-1/2 p-5 flex flex-col lg:flex-row justify-between items-center">
-                <i class="far fa-file-code fa-3x pb-5 lg:pb-0 text-blue-800"></i>
-                <p class="px-0 lg:px-8 text-center lg:text-left">
+            <div class="w-full lg:w-1/2 p-5 flex justify-between">
+                <i class="far fa-file-code fa-3x px-5 lg:px-0 lg:pb-0 text-blue-800 text-blue-800"></i>
+                <p class="px-0 lg:px-8 text-left">
                     Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.
                 </p>
             </div>
