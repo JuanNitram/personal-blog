e3a6e27ef523f06ac87064754de094125d7a4263
Juan <juan@lightit.io>
2020-05-07 17:17:38 -0300

    Add menu dropdown

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -6,11 +6,11 @@
             <!-- <span class="font-semibold text-xl tracking-tight">Juan Vargas</span> -->
         </div>
         <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-gray-400 border-gray-400 hover:text-white hover:border-white">
+            <button class="flex items-center px-3 py-2 border rounded text-gray-400 border-gray-400 hover:text-white hover:border-white" @click="handleButtonClick">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
-        <div class="w-full hidden lg:flex lg:items-center lg:w-auto">
+        <div v-show="showDropdown === true" class="w-full lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
                 <nuxt-link to="/" class="block mt-4 lg:inline-block lg:mt-0 text-gray-400 hover:text-white mr-4">
                     About Me
@@ -28,6 +28,22 @@
         </div>
     </nav>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                showDropdown: false
+            }
+        },
+
+        methods: {
+            handleButtonClick() {
+                this.showDropdown = !this.showDropdown;
+            }
+        }
+    }
+</script>
 
 <style>
     #navbar {
