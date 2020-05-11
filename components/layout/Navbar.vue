71938855db20d3082a2ec446dca463fc8f607a91
Juan <juan@lightit.io>
2020-05-11 09:08:45 -0300

    Add flickity carrousel

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -2,12 +2,12 @@
     <nav id="navbar" class="flex items-center justify-between flex-wrap bg-transparent p-6">
         <div class="flex items-center flex-shrink-0 text-gray-400 mr-6">
             <nuxt-link to="/">
-                <img src="~assets/img/logo-3.svg" class="h-12"/>
+                <img src="~assets/img/logo-2.svg" class="h-12"/>
             </nuxt-link>
         </div>
         <div class="block lg:hidden">
             <button class="flex items-center px-3 py-2 rounded text-gray-400 border-gray-400 hover:text-white hover:border-white" @click="handleButtonClick">
-                <svg class="fill-current h-8 w-8 text-gray-100" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                <svg class="fill-current h-8 w-8 text-blue-800" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
 
@@ -35,7 +35,7 @@
                 <div class="modal-container fixed w-full h-full z-50 overflow-y-auto ">
                     
                     <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-6 mr-6 text-black text-sm z-50" @click="showDropdown = false">
-                        <svg class="fill-current text-gray-100" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 18 18">
+                        <svg class="fill-current text-blue-800" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 18 18">
                             <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
                         </svg>
                     </div>
@@ -43,23 +43,23 @@
                     <div class="modal-content h-full container mx-auto h-auto text-left p-4">
                         <div class="flex h-full justify-between items-center">
                             <div class="block mx-auto">
-                                <p class="w-full block text-4xl font-bold text-center text-gray-100">
+                                <p class="w-full block text-4xl font-bold text-center text-blue-800 mb-5">
                                     <a data-to="/" @click="handleRedirect">
                                         About Me
                                         
                                     </a>
                                 </p>
-                                <p class="w-full block text-4xl font-bold text-center text-gray-100">
+                                <p class="w-full block text-4xl font-bold text-center text-blue-800 mb-5">
                                     <a data-to="/resume" @click="handleRedirect">
                                         Resume
                                     </a>
                                 </p>
-                                <!-- <p class="w-full block text-4xl font-bold text-center text-gray-100">
+                                <!-- <p class="w-full block text-4xl font-bold text-center text-blue-800">
                                     <a data-to="/blog" @click="handleRedirect">
                                         Blog
                                     </a>
                                 </p> -->
-                                <p class="w-full block text-4xl font-bold text-center text-gray-100">
+                                <p class="w-full block text-4xl font-bold text-center text-blue-800 mb-5">
                                     <a data-to="/contact" @click="handleRedirect">
                                         Contact
                                     </a>
@@ -89,7 +89,9 @@
             handleRedirect(event) {
                 event.preventDefault();
                 this.$router.push(event.target.dataset['to']);
-                this.showDropdown = false;
+                setTimeout(() => {
+                    this.showDropdown = false;
+                }, 400)
             }
         }
     }
--- components/pages/home/Stack.vue
+++ components/pages/home/Stack.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="stack" class="mx-auto px-2 lg:px-32 pb-16 text-gray-400">
+    <div id="stack" class="mx-auto px-2 lg:px-32 pb-16 pt-12 text-gray-400">
         <div class="w-full px-5 pb-10">
             <Title :text="$t('stack')"/>
         </div>
-        <div class="block lg:flex px-0 lg:px-5">
+        <div class="hidden lg:flex px-0 lg:px-5">
             <div class="w-full block lg:flex justify-center">
                 <div class="flex justify-center mx-0 lg:mx-12 mb-5 lg:mb-0">
                     <i class="fab fa-laravel fa-5x"></i>
@@ -22,16 +22,16 @@
                 </div>
             </div>
         </div>
-        <!-- <div class="px-0 lg:px-5">
+        <div class="block lg:hidden px-0 lg:px-5">
             <no-ssr>
-                <Flickity>
+                <Flickity :options="options">
                     <div class="carousel-cell"><i class="fab fa-laravel fa-5x"></i></div>
                     <div class="carousel-cell"><i class="fab fa-node fa-5x"></i></div>
                     <div class="carousel-cell"><i class="fab fa-js fa-5x"></i></div>
                     <div class="carousel-cell"><i class="fab fa-vuejs fa-5x"></i></div>
                 </Flickity>
             </no-ssr>
-        </div> -->
+        </div>
     </div>
 </template>
 
@@ -41,6 +41,25 @@
     export default {
         components: {
             Title
+        },
+
+        data() {
+            return {
+                options: {
+                    autoPlay: 3000,
+                    pageDots: false,
+                    prevNextButtons: false,
+                    wrapAround: true,
+                }
+            }
         }
     }
 </script>
+
+<style scoped>
+    .carousel-cell {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+    }
+</style>
