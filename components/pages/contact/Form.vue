221fa3285d6ffd6ae4258b823c4f2f4fd6aa2ce2
Juan <juan@lightit.io>
2020-05-12 16:59:44 -0300

    Add v-models to form inputs

--- components/pages/contact/Form.vue
+++ components/pages/contact/Form.vue
@@ -3,18 +3,17 @@
         <form @submit.prevent="handleSubmit">
             <div class="block lg:flex">
                 <div class="w-full lg:w-1/2 pr-0 lg:pr-2">
-                    <input v-validate="'required'" id="name" name="name" class="w-full p-4 h-10 bg-gray-850 text-gray-100 border-2 border-gray-700 rounded-lg" placeholder="Full name"/>
+                    <input v-model="form.name" v-validate="'required'" id="name" name="name" class="w-full p-4 h-10 bg-gray-850 text-gray-100 border-2 border-gray-700 rounded-lg" placeholder="Full name"/>
                     <span v-show="errors.has('name')" class="ml-1 text-xs text-red-600">{{ errors.first('name') }}</span>
-                    <input v-validate="'required|email'" id="email" name="email" class="w-full p-4 mt-4 h-10 bg-gray-850 text-gray-100 border-2 border-gray-700 rounded-lg" placeholder="Email"/>
+                    <input v-model="form.email" v-validate="'required|email'" id="email" name="email" class="w-full p-4 mt-4 h-10 bg-gray-850 text-gray-100 border-2 border-gray-700 rounded-lg" placeholder="Email"/>
                     <span v-show="errors.has('email')" class="ml-1 text-xs text-red-600">{{ errors.first('email') }}</span>
-                    <input v-validate="'required'" id="phone" name="phone" class="w-full p-4 mt-4 h-10 bg-gray-850 text-gray-100 border-2 border-gray-700 rounded-lg" placeholder="Phone"/>
+                    <input v-model="form.phone" v-validate="'required'" id="phone" name="phone" class="w-full p-4 mt-4 h-10 bg-gray-850 text-gray-100 border-2 border-gray-700 rounded-lg" placeholder="Phone"/>
                     <span v-show="errors.has('phone')" class="ml-1 text-xs text-red-600">{{ errors.first('phone') }}</span>
                 </div>
                 <div class="w-full lg:w-1/2 pl-0 lg:pl-2 flex justify-center items-center">
-                    <textarea v-validate="'required'" id="message" name="message" class="w-full p-4 mt-4 lg:mt-0 h-full bg-gray-850 text-gray-100 border-2 border-gray-700 rounded-lg" placeholder="Message"></textarea>
+                    <textarea v-model="form.message" v-validate="'required'" id="message" name="message" class="w-full p-4 mt-4 lg:mt-0 h-full bg-gray-850 text-gray-100 border-2 border-gray-700 rounded-lg" placeholder="Message"></textarea>
                 </div>
-                    <span v-show="errors.has('message')" class="ml-1 text-xs text-red-600">{{ errors.first('message') }}</span>
-
+                <span v-show="errors.has('message')" class="ml-1 text-xs text-red-600">{{ errors.first('message') }}</span>
             </div>
             <div class="block lg:flex pt-5">
                 <div class="g-recaptcha rounded-lg" data-sitekey="6LclE_MUAAAAAFi3KKCAOGXhGFCRbkaIyvaOjU6z" data-theme="dark"></div>
@@ -41,7 +40,8 @@
                     email: '',
                     phone: '',
                     message: '',
-                }
+                },
+                loading: false,
             }
         },
 
@@ -49,8 +49,17 @@
             handleSubmit() {
                 this.$validator.validateAll().then((isValid) => {
                     if(isValid === true){
-                        console.log(this);
-                        this.$axios.post('/contact/send', this.form)
+                        this.loading = true;
+
+                        this.$axios.post('/contact/send', this.form).then(({ data }) => {
+                            if(data.status === 'success'){
+                                
+                            }
+                        }).catch(ex => {
+                            console.log(ex);
+                        }).finally(() => {
+                            this.loading = false;
+                        })
                     }
                 })
             },
