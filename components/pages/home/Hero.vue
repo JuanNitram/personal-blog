f5c6020443e67ef1e338a8660f137966205c02cf
Juan <juan@lightit.io>
2020-05-13 15:58:51 -0300

    Add full height hero section

--- components/pages/home/Hero.vue
+++ components/pages/home/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="hero" class="block lg:flex mx-auto px-5 md:px-24 lg:px-32 xl:px-72 pb-20 lg:pb-12 pt-12">
+    <div id="hero" class="block lg:flex mx-auto px-5 md:px-24 lg:px-32 xl:px-72 pb-20 lg:pb-12 pt-12 xl:pt-0 mt-0 xl:-mt-12">
         <div class="w-full lg:w-1/2 flex flex-col justify-center items-center py-6">
           <img id="personal-image" src="~/assets/img/me-3.jpeg" class="rounded-full border-gray-700 w-64 h-64 lg:w-72 lg:h-72 xl:w-96 xl:h-96 shadow-lg"/>
         </div>
@@ -31,5 +31,11 @@
   #personal-image {
     border-width: 14px;
     box-shadow: 0 0 25px 0 rgba(0,0,0,.8)
+  }
+
+  @media (min-width: 1280px) { 
+    #hero {
+      height: 100vh;
+    }
   }
 </style>
