04852446851e27cbbafa177097d1e24c94e2a542
Juan Vargas <juanmvg2006@gmail.com>
2020-09-11 15:59:02 -0300

    Update profile photo

--- components/pages/home/Hero.vue
+++ components/pages/home/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="hero" class="block lg:flex mx-auto px-5 md:px-24 lg:px-32 xl:px-72 pb-20 lg:pb-12 pt-12 xl:pt-0 mt-0 xl:-mt-12">
         <div class="w-full lg:w-1/2 flex flex-col justify-center items-center py-6">
-          <img id="personal-image" src="~/assets/img/me-3.jpeg" class="rounded-full border-gray-700 w-64 h-64 lg:w-72 lg:h-72 xl:w-96 xl:h-96 shadow-lg"/>
+          <img id="personal-image" src="~/assets/img/me-4.JPG" class="rounded-full border-gray-700 w-64 h-64 lg:w-72 lg:h-72 xl:w-96 xl:h-96 shadow-lg"/>
         </div>
         <div class="w-full lg:w-1/2 flex flex-col justify-center">
             <h3 class="text-lg lg:text-xl text-center lg:text-left text-gray-400">Juan Vargas</h3>
@@ -33,7 +33,7 @@
     box-shadow: 0 0 25px 0 rgba(0,0,0,.8)
   }
 
-  @media (min-width: 1280px) { 
+  @media (min-width: 1280px) {
     #hero {
       height: 100vh;
     }
