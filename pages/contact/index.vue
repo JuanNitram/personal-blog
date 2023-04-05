b2db41b0c9e1b338f05ef13ea53d8956c55a2ed1
Juan Vargas <juanmvg2006@gmail.com>
2023-04-05 19:24:06 -0300

    Update contact page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,14 +1,8 @@
 <template>
-    <div id="contact" class="pt-12">
+    <div id="contact" class="pt-12 h-screen">
         <Breadcrumb text="Contact" secondary-text="Get in Touch"/>
-
-        <div class="flex px-4 lg:px-24 xl:px-40 py-10 pt-16 lg:pt-20 lg:py-20">
-            <div class="w-full">
-                <div style="width: 100%"><iframe class="rounded-lg" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d818.0371367853747!2d-56.1838604707791!3d-34.902721498900725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f80352fe8f1fd%3A0xab77267017cba639!2sMercedes%20%26%20Calle%20Tacuaremb%C3%B3%2C%2011200%20Montevideo%2C%20Departamento%20de%20Montevideo!5e0!3m2!1sen!2suy!4v1589383833750!5m2!1sen!2suy" width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div><br />
-            </div>
-        </div>
         
-        <div class="block lg:flex px-4 lg:px-24 xl:px-40 pb-20">
+        <div class="block lg:flex px-4 lg:px-24 xl:px-40 py-20">
             <div class="w-full lg:w-1/3 pr-0 lg:pr-6" data-aos="fade-up">
                 <div class="flex">
                     <div class="flex flex-col justify-center p-2 lg:p-0">
